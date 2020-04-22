a560f9e7a8cd575b9db3c5b2d23e1db563326b1d
JSkills <jskills@gmail.com>
2020-04-22 18:04:09 -0400

    some new methods

--- findMp3s.py
+++ findMp3s.py
@@ -4,6 +4,7 @@ import eyed3
 import psycopg2
 import re
 import string
+import glob
 from configparser import ConfigParser
 from unidecode import unidecode
 import warnings
@@ -11,24 +12,16 @@ import warnings
 warnings.simplefilter("ignore")
 eyed3.log.setLevel("ERROR")
 
-debug = False
-runLimit = 0
-
-# if you need to debug
-#import pdb
-# then add this to where you want to set a break
-# pdb.set_trace()
-
 
 #######################
 
-def search_files(directory='.', extension=''):
+def search_files(directory='.', extension='', skip=None):
 	fileCount = 0
 	returnList = list()
 	extension = extension.lower()
 	for dirpath, dirnames, files in os.walk(directory):
-		# skip this directory for now
-		dirnames[:] = [d for d in dirnames if d != "Kenans Treasure Chest"]
+		if skip:
+			dirnames[:] = [d for d in dirnames if d != skip]
 		for name in files:
 			if extension and name.lower().endswith(extension):
 				songFile = os.path.join(dirpath, name)
@@ -214,169 +207,217 @@ def getResults(conn, sql, type='list'):
 ###
 
 
+def syncDB(songList, debug=False, runLimit=0):
+
+	conn = connect()
+
+	genres = populateGenres(conn)
+
+	i = 0
+	totalProcessed = 0
+	while i < len(songList):
+		songDict = songList[i]
+		i += 1
+
+		#eyed3 genre : looks like (42)Soul
+		if songDict['genre']:
+			songDict['genre'] = re.sub('\([^()]*\)', '', songDict['genre'])
+		# lookup genreId
+		songDict['genre_id'] = returnGenreID(genres, songDict['genre'])
+
+		if songDict['artist']:
+			songDict['artist_id'] = returnArtistID(conn, songDict['artist'])
+			if not songDict['artist_id']:
+				sql = "insert into artist (full_name, last_updated_by) values (%s, %s) returning id"
+				try:
+					cur = conn.cursor()
+					cur.execute(sql, (songDict['artist'], 'jskills'))
+					songDict['artist_id'] = cur.fetchone()[0]
+				except:
+					print("Cannot find or insert artist :" + normalizeUnicode(songDict['artist']))
+		else:
+			print("No MP3 tag for artist for " + str(songDict['filename']))
+			continue
+		if not songDict['artist_id']:
+			print("No artist ID for " + str(songDict['filename']))
+			continue
+
+		# we have artist and genre so let's decide whether to insert a new song or update an existing one
+		# remove musicDir from file_path prior to putting into the DB
+		songDict['file_path'] = re.sub(musicDir,'', songDict['filename'])
+		sql = "select id from song where file_path = (%s)"
+		if debug:
+			print(sql + normalizeUnicode(songDict['file_path']))
+		cur = conn.cursor()
+		try:
+			cur.execute(sql, (normalizeUnicode(songDict['file_path']),))
+			songDict['song_id'] = cur.fetchone()[0]
+		except:
+			print("Had to skip this one for now : " + normalizeUnicode(songDict['file_path']))
+			continue
+
+		sqlList = list()
+ 		# these fields should always be present
+		sqlList.append(songDict['artist_id'])
+		sqlList.append(songDict['title'])
+		sqlList.append(songDict['file_path'])
+		sqlList.append(songDict['genre_id'])
+		sqlList.append(songDict['year'])
+		sqlList.append('jskills')
+
+		if songDict['song_id']:
+			# update song meta data
+			sql = "update song set artist_id = %s, song_name = %s, file_path = %s, genre_id = %s, year = %s, last_updated_by = %s"
+			if 'comment' in songDict.keys():
+				if songDict['comment'] and songDict['comment'] != 'None':
+					sql += ", comment = %s"
+					sqlList.append(songDict['comment'])
+			if 'album' in songDict.keys():
+				if songDict['album'] and songDict['album'] != 'None':
+					sql += ", album = %s"
+					sqlList.append(songDict['album'])
+			if 'track_number' in songDict.keys():
+				if songDict['track_number'] > 0 and songDict['track_number'] != 'None':
+					sql += ", track_number = %s"
+					sqlList.append(songDict['track_number'])
+			if 'bit_rate' in songDict.keys():
+				if int(songDict['bit_rate']) > 0 and songDict['bit_rate'] != 'None':
+					sql += ", bit_rate = %s"
+					sqlList.append(songDict['bit_rate'])
+	
+			sql += ", last_updated_date = now() "	
+			sql += " where id = %s"
+			sqlList.append(songDict['song_id'])
+
+			if debug:
+				print(sql)
+				print(sqlList)
+			else:
+				try:
+					cur = conn.cursor()
+					cur.execute(sql, sqlList)
+					conn.commit()
+				except:
+					print("Update failed for " + str(songDict['filename']))
+					print(sql)
+					print(sqlList)
+					continue
+			
+		else:
+			# insert new song
+			sql1 = "insert into song (artist_id, song_name, file_path, genre_id, year, last_updated_by"
+			sql2 = " ) values (%s,%s,%s,%s,%s,%s"
+
+			if 'comment' in songDict.keys():
+				if songDict['comment'] and songDict['comment'] != 'None':
+					sql1 += ", comment"
+					sql2 += ",%s"
+					sqlList.append(songDict['comment'])
+			if 'album' in songDict.keys():
+				if songDict['album'] and songDict['album'] != 'None':
+					sql1 += ", album"
+					sql2 += ",%s"
+					sqlList.append(songDict['album'])
+			if 'track_number' in songDict.keys():
+				if songDict['track_number'] > 0 and songDict['track_number'] != 'None':
+					sql1 += ",track_number"
+					sql2 += ",%s"
+					sqlList.append(songDict['track_number'])
+			if 'bit_rate' in songDict.keys():
+				if int(songDict['bit_rate']) > 0 and songDict['bit_rate'] != 'None':
+					sql1 += ",bit_rate"
+					sql2 += ",%s"
+					sqlList.append(songDict['bit_rate'])
+			sql2 += ")"
+			sql = sql1 + sql2 + " RETURNIND id"
+
+			if debug:
+				print(sql)
+				print(sqlList)
+			else:
+				try:
+					cur = conn.cursor()
+					cur.execute(sql, sqlList)
+					songDict['song_id'] = cur.fetchone()[0]
+					conn.commit()
+				except:
+					print("Insert failed for " + str(songDict['filename']))
+					print(sql)
+					print(sqlList)
+					continue
+
+		writeCover(songDict)
+
+		totalProcessed += 1
+
+	conn.close()
+
+	return totalProcessed
+
+###
+
+def purgeOrphans(songList, debug=False):
+
+	conn = connect()
+	sql = "select id, song_name, file_path from song"
+	allSongs = getResults(conn, sql)
+	
+	removed = 0	
+	for s in allSongs:
+		checkPath = musicDir + str(s['file_path'])
+		if not os.path.exists(checkPath):
+			print("Deleting record for song - id : " + str(s['id']) + " name " + str(s['id']))
+			print(str(checkPath) + " does not exist.")
+			sql = "delete from songs where id = %s"
+			if debug:
+				print(sql)
+			else:
+				print(sql)
+				# need to address DB lookup of filenames before we can be sure to delete
+				#cur.execute(sql, sqlList)
+				#conn.commit()
+				
+			removed += 1
+	conn.close()
+
+	return removed
+	
 #######################
 
+
+
+# set locally 
 musicDir = "/media/jskills/Toshiba-2TB/"
 coverImageDir = musicDir + "cover_art/"
 
-conn = connect()
+# debug flag will result in printing SQL rather than doing any inserts / updates
+debug = True
 
-genres = populateGenres(conn)
+# set to halt after processing a certain number of records in dbSync, leave 0 to process all 
+runLimit = 0
 
+# locate all mp3 files and exclude ones you're not ready to process into the database yet
+excludeDir = "Kenans Treasure Chest"
+songList = search_files(musicDir, '.mp3', excludeDir)
 
-songList = search_files(musicDir, '.mp3')
-
-# look up song ID based on file name
-# look up artist in artist table in DB
-# if found use artist_id, if not insert new artist and use that ID
-# lookup song/artist combo in song table in DB
-
-
-i = 0
-totalProcessed = 0
-while i < len(songList):
-	songDict = songList[i]
-	i += 1
-
-	#eyed3 genre : looks like (42)Soul
-	if songDict['genre']:
-		songDict['genre'] = re.sub('\([^()]*\)', '', songDict['genre'])
-	# lookup genreId
-	songDict['genre_id'] = returnGenreID(genres, songDict['genre'])
-
-	if songDict['artist']:
-		songDict['artist_id'] = returnArtistID(conn, songDict['artist'])
-		if not songDict['artist_id']:
-			sql = "insert into artist (full_name, last_updated_by) values (%s, %s) returning id"
-			try:
-				cur = conn.cursor()
-				cur.execute(sql, (songDict['artist'], 'jskills'))
-				songDict['artist_id'] = cur.fetchone()[0]
-			except:
-				print("Cannot find or insert artist :" + normalizeUnicode(songDict['artist']))
-	else:
-		print("No MP3 tag for artist for " + str(songDict['filename']))
-		continue
-	if not songDict['artist_id']:
-		print("No artist ID for " + str(songDict['filename']))
-		continue
-
-	# we have artist and genre so let's decide whether to insert a new song or update an existing one
-	# remove musicDir from file_path prior to putting into the DB
-	songDict['file_path'] = re.sub(musicDir,'', songDict['filename'])
-	sql = "select id from song where file_path = (%s)"
-	if debug:
-		print(sql + normalizeUnicode(songDict['file_path']))
-	cur = conn.cursor()
-	try:
-		cur.execute(sql, (normalizeUnicode(songDict['file_path']),))
-		songDict['song_id'] = cur.fetchone()[0]
-	except:
-		print("Had to skip this one for now : " + normalizeUnicode(songDict['file_path']))
-		continue
-
-	sqlList = list()
- 	# these fields should always be present
-	sqlList.append(songDict['artist_id'])
-	sqlList.append(songDict['title'])
-	sqlList.append(songDict['file_path'])
-	sqlList.append(songDict['genre_id'])
-	sqlList.append(songDict['year'])
-	sqlList.append('jskills')
-
-	if songDict['song_id']:
-		# update song meta data
-		sql = "update song set artist_id = %s, song_name = %s, file_path = %s, genre_id = %s, year = %s, last_updated_by = %s"
-		if 'comment' in songDict.keys():
-			if songDict['comment'] and songDict['comment'] != 'None':
-				sql += ", comment = %s"
-				sqlList.append(songDict['comment'])
-		if 'album' in songDict.keys():
-			if songDict['album'] and songDict['album'] != 'None':
-				sql += ", album = %s"
-				sqlList.append(songDict['album'])
-		if 'track_number' in songDict.keys():
-			if songDict['track_number'] > 0 and songDict['track_number'] != 'None':
-				sql += ", track_number = %s"
-				sqlList.append(songDict['track_number'])
-		if 'bit_rate' in songDict.keys():
-			if int(songDict['bit_rate']) > 0 and songDict['bit_rate'] != 'None':
-				sql += ", bit_rate = %s"
-				sqlList.append(songDict['bit_rate'])
-	
-		sql += ", last_updated_date = now() "	
-		sql += " where id = %s"
-		sqlList.append(songDict['song_id'])
-
-		if debug:
-			print(sql)
-			print(sqlList)
-		else:
-			try:
-				cur = conn.cursor()
-				cur.execute(sql, sqlList)
-				conn.commit()
-			except:
-				print("Update failed for " + str(songDict['filename']))
-				print(sql)
-				print(sqlList)
-				continue
-			
-	else:
-		# insert new song
-		sql1 = "insert into song (artist_id, song_name, file_path, genre_id, year, last_updated_by"
-		sql2 = " ) values (%s,%s,%s,%s,%s,%s"
-
-		if 'comment' in songDict.keys():
-			if songDict['comment'] and songDict['comment'] != 'None':
-				sql1 += ", comment"
-				sql2 += ",%s"
-				sqlList.append(songDict['comment'])
-		if 'album' in songDict.keys():
-			if songDict['album'] and songDict['album'] != 'None':
-				sql1 += ", album"
-				sql2 += ",%s"
-				sqlList.append(songDict['album'])
-		if 'track_number' in songDict.keys():
-			if songDict['track_number'] > 0 and songDict['track_number'] != 'None':
-				sql1 += ",track_number"
-				sql2 += ",%s"
-				sqlList.append(songDict['track_number'])
-		if 'bit_rate' in songDict.keys():
-			if int(songDict['bit_rate']) > 0 and songDict['bit_rate'] != 'None':
-				sql1 += ",bit_rate"
-				sql2 += ",%s"
-				sqlList.append(songDict['bit_rate'])
-		sql2 += ")"
-		sql = sql1 + sql2 + " RETURNIND id"
-
-		if debug:
-			print(sql)
-			print(sqlList)
-		else:
-			try:
-				cur = conn.cursor()
-				cur.execute(sql, sqlList)
-				songDict['song_id'] = cur.fetchone()[0]
-				conn.commit()
-			except:
-				print("Insert failed for " + str(songDict['filename']))
-				print(sql)
-				print(sqlList)
-				continue
-
-	writeCover(songDict)
-
-	totalProcessed += 1
-
-print("Completed processing " + str(totalProcessed) + " files.")
+# synchronize database to reflect songs on disk
+#songsProcessed = syncDB(songList, debug, runLimit)
+#print("Completed processing " + str(songsProcessed) + " files.")
 
 
-
-# now that we have ensured all existing songs and new songs are in the database, we should scan all songs and ensure the files are still on disk
+# now that we have ensured all existing songs and new songs are in the database,
+# we should scan all songs and ensure the files are still on disk
 # if not, we should delete the record in the song table
+orphansProcessed = purgeOrphans(songList, debug)
+print("Removed " + str(orphansProcessed) + " orphaned DB records")
 
-# also we need to cleanup all files in the cover images directory that do not start with a number (hence not the cover image for the song_id
-
+# also we need to cleanup all files in the cover images directory that do not start with a number
+# (hence not the cover image for the song_id)
+deleteList = glob.glob(coverImageDir + "/[A-Z]*")
+for dl in deleteList:
+	if debug:
+		print(str(dl))
+	else:
+		os.remove(dl)
 
 
